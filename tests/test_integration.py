4c056e1be840f576247ba9f0466980ec96d4a714
Luciana Araújo <83361295+Lucianaaraujo1208@users.noreply.github.com>
2024-09-19 13:09:47 -0300

    registro e criação de novo usuario

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,7 +4,7 @@ from todo_project.models import User
 
 @pytest.fixture
 def client():
-    """Configura o cliente de teste e o banco de dados em memória."""
+    """Configurar o cliente de teste e o banco de dados em memória."""
     app.config['TESTING'] = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
     client = app.test_client()
