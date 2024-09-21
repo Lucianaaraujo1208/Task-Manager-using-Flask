d34eb55d3634103effabe5fd01d2d0dac4304f06
Luciana Araújo <83361295+Lucianaaraujo1208@users.noreply.github.com>
2024-09-21 01:40:44 -0300

    mitigação vulnerabilidade 3

--- todo_project/routes.py
+++ todo_project/routes.py
@@ -1,36 +1,34 @@
-from flask import render_template, url_for, flash, redirect, request
-
+from flask import render_template, url_for, flash, redirect, request, escape
 from todo_project import app, db, bcrypt
-
-# Import the forms
 from todo_project.forms import (LoginForm, RegistrationForm, UpdateUserInfoForm, 
                                 UpdateUserPassword, TaskForm, UpdateTaskForm)
-
-# Import the Models
 from todo_project.models import User, Task
+from flask_login import login_required, current_user, login_user, logout_user
 
-# Import 
-from flask_login import login_required, current_user, login_user, logout_user  # type: ignore
-
+@app.after_request
+def set_security_headers(response):
+    response.headers['Content-Security-Policy'] = "default-src 'self';"
+    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['Permissions-Policy'] = 'geolocation=(self)'
+    return response
 
 @app.errorhandler(404)
 def error_404(error):
-    return (render_template('errors/404.html'), 404)
+    return render_template('errors/404.html'), 404
 
 @app.errorhandler(403)
 def error_403(error):
-    return (render_template('errors/403.html'), 403)
+    return render_template('errors/403.html'), 403
 
 @app.errorhandler(500)
 def error_500(error):
-    return (render_template('errors/500.html'), 500)
-
+    return render_template('errors/500.html'), 500
 
 @app.route("/")
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
-
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
@@ -38,26 +36,21 @@ def login():
         return redirect(url_for('all_tasks'))
 
     form = LoginForm()
-    # After you submit the form
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        # Check if the user exists and the password is valid
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
-            task_form = TaskForm()
-            flash('Login Successfull', 'success')
+            flash('Login Successful', 'success')
             return redirect(url_for('all_tasks'))
         else:
             flash('Login Unsuccessful. Please check Username Or Password', 'danger')
     
     return render_template('login.html', title='Login', form=form)
-    
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('login'))
-
 
 @app.route("/register", methods=['POST', 'GET'])
 def register():
@@ -67,14 +60,13 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, password=hashed_password)
+        user = User(username=escape(form.username.data), password=hashed_password)  # Escape username
         db.session.add(user)
         db.session.commit()
-        flash(f'Account Created For {form.username.data}', 'success')
+        flash(f'Account Created For {escape(form.username.data)}', 'success')  # Escape username
         return redirect(url_for('login'))
 
     return render_template('register.html', title='Register', form=form)
-
 
 @app.route("/all_tasks")
 @login_required
@@ -82,19 +74,17 @@ def all_tasks():
     tasks = User.query.filter_by(username=current_user.username).first().tasks
     return render_template('all_tasks.html', title='All Tasks', tasks=tasks)
 
-
 @app.route("/add_task", methods=['POST', 'GET'])
 @login_required
 def add_task():
     form = TaskForm()
     if form.validate_on_submit():
-        task = Task(content=form.task_name.data, author=current_user)
+        task = Task(content=escape(form.task_name.data), author=current_user)  # Escape task content
         db.session.add(task)
         db.session.commit()
         flash('Task Created', 'success')
         return redirect(url_for('add_task'))
     return render_template('add_task.html', form=form, title='Add Task')
-
 
 @app.route("/all_tasks/<int:task_id>/update_task", methods=['GET', 'POST'])
 @login_required
@@ -103,7 +93,7 @@ def update_task(task_id):
     form = UpdateTaskForm()
     if form.validate_on_submit():
         if form.task_name.data != task.content:
-            task.content = form.task_name.data
+            task.content = escape(form.task_name.data)  # Escape updated task content
             db.session.commit()
             flash('Task Updated', 'success')
             return redirect(url_for('all_tasks'))
@@ -114,7 +104,6 @@ def update_task(task_id):
         form.task_name.data = task.content
     return render_template('add_task.html', title='Update Task', form=form)
 
-
 @app.route("/all_tasks/<int:task_id>/delete_task")
 @login_required
 def delete_task(task_id):
@@ -124,14 +113,13 @@ def delete_task(task_id):
     flash('Task Deleted', 'info')
     return redirect(url_for('all_tasks'))
 
-
 @app.route("/account", methods=['POST', 'GET'])
 @login_required
 def account():
     form = UpdateUserInfoForm()
     if form.validate_on_submit():
         if form.username.data != current_user.username:  
-            current_user.username = form.username.data
+            current_user.username = escape(form.username.data)  # Escape username update
             db.session.commit()
             flash('Username Updated Successfully', 'success')
             return redirect(url_for('account'))
@@ -139,7 +127,6 @@ def account():
         form.username.data = current_user.username 
 
     return render_template('account.html', title='Account Settings', form=form)
-
 
 @app.route("/account/change_password", methods=['POST', 'GET'])
 @login_required
@@ -150,9 +137,8 @@ def change_password():
             current_user.password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
             db.session.commit()
             flash('Password Changed Successfully', 'success')
-            redirect(url_for('account'))
+            return redirect(url_for('account'))
         else:
-            flash('Please Enter Correct Password', 'danger') 
+            flash('Please Enter Correct Password', 'danger')
 
     return render_template('change_password.html', title='Change Password', form=form)
-
