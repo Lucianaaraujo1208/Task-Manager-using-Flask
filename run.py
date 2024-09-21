b38986aab2994fa9f66868a86c64e3fba7d55419
Luciana Araújo <83361295+Lucianaaraujo1208@users.noreply.github.com>
2024-09-21 01:26:08 -0300

    mitigação vulnerabilidade 2

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
 from todo_project import app
+from flask import Flask, request
+
+# Função para adicionar cabeçalhos de segurança
+@app.after_request
+def set_security_headers(response):
+    response.headers['Content-Security-Policy'] = "default-src 'self';"
+    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['Permissions-Policy'] = 'geolocation=(self)'
+    return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Rodar o app sem o modo de depuração
+    app.run(host='0.0.0.0', port=5000)  # Mude o host e a porta conforme necessário
